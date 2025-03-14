8262aaa6b8657c91bd8c0de45be6f3650acdb129
Vishnu Prasad <91651133+vishnu-focaloid@users.noreply.github.com>
2025-03-14 14:43:17 +0530

    custom patch for cve-2024-10041

--- modules/pam_unix/passverify.c
+++ modules/pam_unix/passverify.c
@@ -194,17 +194,22 @@ PAMH_ARG_DECL(int get_account_info,
 			return PAM_UNIX_RUN_HELPER;
 #endif
 		} else if (is_pwd_shadowed(*pwd)) {
+#ifndef HELPER_COMPILE
 			/*
 			 * ...and shadow password file entry for this user,
 			 * if shadowing is enabled
 			 */
-#ifndef HELPER_COMPILE
-			if (geteuid() || SELINUX_ENABLED)
-				return PAM_UNIX_RUN_HELPER;
-#endif
-			*spwdent = pam_modutil_getspnam(pamh, name);
-			if (*spwdent == NULL || (*spwdent)->sp_pwdp == NULL)
+			 *spwdent = getspnam(name);
+			 if (*spwdent == NULL || (*spwdent)->sp_pwdp == NULL)
 				return PAM_AUTHINFO_UNAVAIL;
+#else
+			/*
+			* The helper has to be invoked to deal with
+			* the shadow password file entry.
+			*/
+
+			return PAM_UNIX_RUN_HELPER;
+#endif
 		}
 	} else {
 		return PAM_USER_UNKNOWN;
